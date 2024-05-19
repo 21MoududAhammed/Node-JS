b3fa6f07b885aefd1fa3d5cd174bc61f08db39ac
21MoududAhammed <21moudud@gmail.com>
2024-05-19 21:11:11 +0600

    To learn how node js receive data by steaming

--- stream-buffer/index.js
+++ stream-buffer/index.js
@@ -1,8 +1,36 @@
-const fs = require("fs");
+const http = require("http");
 
-const ourReadSteam = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
-
-ourReadSteam.on("data", (data) => {
-  console.log(data);
-  console.log('//////////');
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write(`
+        <html>
+            <head>
+                <title>Form</title>
+            </head>
+         <body>
+            <form method='post' action='/process'>
+                <input name='message'/>
+            </form>
+        </body>
+        </html>`);
+    res.end();
+  } else if (req.url === "/process" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      console.log("Steam Finished!");
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+      res.write("Thank you for submitting.");
+      res.end();
+    });
+  } else {
+    res.write("Not Found!");
+  }
 });
+
+server.listen(3000);
+
+console.log("Listening port on 3000");
